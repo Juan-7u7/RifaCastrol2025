5e112489f1f23cfafe05a9dc954f6aa531cedbec
juanhdztr <juanhdztr@gmail.com>
2025-10-20 23:41:26 +0000

    vercel ins & anal

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { AuthProvider } from './AuthContext.jsx';
 import { SpeedInsights } from "@vercel/speed-insights/react"; // <-- Bien importado
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthProvider>
         <App />
         <SpeedInsights /> {/* <-- Colócalo aquí, junto a App */}
+        <Analytics/>
       </AuthProvider>
     </BrowserRouter>
     {/* <SpeedInsights/> Quítalo de aquí fuera */}
