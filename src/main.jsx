c151d00ef525b60e247c7bbd49f9a928616ef69f
juanhdztr <juanhdztr@gmail.com>
2025-10-20 23:54:23 +0000

    ash

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { AuthProvider } from './AuthContext.jsx';
-import { SpeedInsights } from "@vercel/speed-insights/react"; // <-- Bien importado
+import { SpeedInsights } from "@vercel/speed-insights/react" 
 import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
